bebd5d8ffe87a880e44d0b6d59e3e8a90d13b3f3
Saransh29 <saranshbibiyan234@gmail.com>
2023-02-08 15:07:27 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full  items-center text-center bg-gray-200 p-3">
+      <h1 className="text-3xl font-bold  ">Next Testing</h1>
+    </div>
+  );
+};
+
+export default page;
